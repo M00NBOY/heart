676ec8fc4d64e4fe2a6403f8135ea02212c88c05
M00NBOY <zeroualmahel@gmail.com>
2018-02-26 13:54:57 +0100

    favicon plus some correction

--- js/app.js
+++ js/app.js
@@ -22,19 +22,19 @@
     );
 
     (function gameLoop() {
-        if (x > 0 && keyState[37] || keyState[81]){
+        if (x > 0 && (keyState[37] || keyState[81])){
             x -= 0.4;
         }
 
-        if (x < 73.1 && keyState[39] || keyState[68]){
+        if (x < 73.1 && (keyState[39] || keyState[68])){
             x += 0.4;
         }
 
-        if (y > 0 && keyState[38] || keyState[90]) {
+        if (y > 0 && (keyState[38] || keyState[90])) {
             y -= 0.4;
         }
 
-        if (y < 53 && keyState[40] || keyState[83]) {
+        if (y < 53 && (keyState[40] || keyState[83])) {
             y += 0.4;
         }
 
